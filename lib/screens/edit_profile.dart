fe7d15a82b35122682db9bf49010ad465dd46c20
Lucas Newman <97490799+Squidly1408@users.noreply.github.com>
2024-02-25 14:21:10 +1100

    debugging

--- lib/screens/edit_profile.dart
+++ lib/screens/edit_profile.dart
@@ -1,11 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-
-import 'dart:io';
 
 import '../home.dart';
 import '../main.dart';
